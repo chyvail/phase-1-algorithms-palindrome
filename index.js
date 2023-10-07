ef83f6ea7c3cdef04aa013914e7db6e4606b8534
joshua chivile <joshuachivile@gmail.com>
2023-10-07 17:17:07 +0300

    created function to check plaindrome

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  let splitString = word.split("");
+  let reversedArray = splitString.reverse();
+  let joinedWord = reversedArray.join("");
+  
+  return (word == joinedWord);
 }
+
+console.log(isPalindrome("racecar"))
 
 /* 
   Add your pseudocode here
+  1. write our function isPalindrome that take in a (word) argument
+  2. split the word into an array, reverse and join it
+  3. create a if statement to check if the word paassed when reversed is still same
+  4. return if output is true or false
 */
 
 /*
